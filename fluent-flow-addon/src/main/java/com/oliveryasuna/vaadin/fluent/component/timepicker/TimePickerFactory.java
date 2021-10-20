7f3d3f6468167bfda9d479232df85f61dda4c56b
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2021-10-20 18:25:21 -0400

    Created factories for com.vaadin:vaadin-time-picker-flow.

---
+++ fluent-flow-addon/src/main/java/com/oliveryasuna/vaadin/fluent/component/timepicker/TimePickerFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 Oliver Yasuna
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.oliveryasuna.vaadin.fluent.component.timepicker;
+
+import com.oliveryasuna.commons.language.fluent.FluentFactory;
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.HasValue.ValueChangeListener;
+import com.vaadin.flow.component.timepicker.TimePicker;
+
+import java.time.LocalTime;
+
+public class TimePickerFactory extends FluentFactory<TimePicker, TimePickerFactory> implements ITimePickerFactory<TimePicker, TimePickerFactory> {
+
+  public TimePickerFactory(final TimePicker timePicker) {
+    super(timePicker);
+  }
+
+  public TimePickerFactory() {
+    super(new TimePicker());
+  }
+
+  public TimePickerFactory(final LocalTime time) {
+    super(new TimePicker(time));
+  }
+
+  public TimePickerFactory(final String label) {
+    super(new TimePicker(label));
+  }
+
+  public TimePickerFactory(final String label, final LocalTime time) {
+    super(new TimePicker(label, time));
+  }
+
+  public TimePickerFactory(final ValueChangeListener<ComponentValueChangeEvent<TimePicker, LocalTime>> listener) {
+    super(new TimePicker(listener));
+  }
+
+}
